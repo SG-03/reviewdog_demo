16e73447016a4b98d6d99dbd63b0a968eed78a00
SG-03 <sruthi.ganjam@gmail.com>
2022-09-01 22:37:36 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name_arr = ["harry", "percy", "jace"];
+  const [names, setNames] = useState(name_arr);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {names.map((name) => (
+        <p key={name}>{name}</p>
+      ))}
     </div>
   );
 }
